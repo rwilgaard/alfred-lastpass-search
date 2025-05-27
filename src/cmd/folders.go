a07268179764ce223920c4d34309ce9823c86451
Alexander Soelberg Heidarsson <89837986+alex5517@users.noreply.github.com>
2025-05-27 06:20:41 +0200

    chore: gofumpt

--- src/cmd/folders.go
+++ src/cmd/folders.go
@@ -7,42 +7,40 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-    foldersCmd = &cobra.Command{
-        Use:          "folders",
-        Short:        "list folders",
-        SilenceUsage: true,
-        Args: cobra.ExactArgs(1),
-        Run: func(cmd *cobra.Command, args []string) {
-            ls, err := lastpass.NewLastpassService("lpass")
-            if err != nil {
-                wf.FatalError(err)
-            }
+var foldersCmd = &cobra.Command{
+	Use:          "folders",
+	Short:        "list folders",
+	SilenceUsage: true,
+	Args:         cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		ls, err := lastpass.NewLastpassService("lpass")
+		if err != nil {
+			wf.FatalError(err)
+		}
 
-            folders, err := ls.GetFolders()
-            if err != nil {
-                wf.FatalError(err)
-            }
+		folders, err := ls.GetFolders()
+		if err != nil {
+			wf.FatalError(err)
+		}
 
-            wf.NewItem("Select folder").
-                Match("*").
-                Subtitle("Type to search").
-                Valid(false)
+		wf.NewItem("Select folder").
+			Match("*").
+			Subtitle("Type to search").
+			Valid(false)
 
-            for _, f := range folders {
-                wf.NewItem(f.Name).
-                    UID(f.Name).
-                    Icon(util.IconFolder).
-                    Var("folder", f.Name).
-                    Valid(true)
-            }
+		for _, f := range folders {
+			wf.NewItem(f.Name).
+				UID(f.Name).
+				Icon(util.IconFolder).
+				Var("folder", f.Name).
+				Valid(true)
+		}
 
-            wf.Filter(args[0])
-            alfredutils.HandleFeedback(wf)
-        },
-    }
-)
+		wf.Filter(args[0])
+		alfredutils.HandleFeedback(wf)
+	},
+}
 
 func init() {
-    rootCmd.AddCommand(foldersCmd)
+	rootCmd.AddCommand(foldersCmd)
 }
--- src/cmd/update.go
+++ src/cmd/update.go
@@ -7,22 +7,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-    updateCmd = &cobra.Command{
-        Use:          "update",
-        Short:        "check for updates",
-        SilenceUsage: true,
-        RunE: func(cmd *cobra.Command, args []string) error {
-            wf.Configure(aw.TextErrors(true))
-            log.Println("Checking for updates...")
-            if err := wf.CheckForUpdate(); err != nil {
-                return err
-            }
-            return nil
-        },
-    }
-)
+var updateCmd = &cobra.Command{
+	Use:          "update",
+	Short:        "check for updates",
+	SilenceUsage: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		wf.Configure(aw.TextErrors(true))
+		log.Println("Checking for updates...")
+		if err := wf.CheckForUpdate(); err != nil {
+			return err
+		}
+		return nil
+	},
+}
 
 func init() {
-    rootCmd.AddCommand(updateCmd)
+	rootCmd.AddCommand(updateCmd)
 }
